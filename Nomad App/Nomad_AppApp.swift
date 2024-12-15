a6ea0c81284372faef7d8fcfb2c976b314c2310f
DUPROS Leegan <leegan.dupros@epitech.eu>
2024-12-15 17:35:02 +0100

    Initial Commit

---
+++ Nomad App/Nomad_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nomad_AppApp.swift
+//  Nomad App
+//
+//  Created by dleegan on 15/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Nomad_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
